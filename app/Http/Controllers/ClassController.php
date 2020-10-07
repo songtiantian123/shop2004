65061bbe94644acba89516270304431080c99291
songtiantian <1968551623@qq.com>
2020-10-07 18:08:11 +0800

    商品详情

---
+++ app/Http/Controllers/ClassController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClassController extends Controller
+{
+    public function class(){
+       // 班级表
+    }
+    public function studebt(){
+        // 班级
+    }
+}
